d6e4e600a1bbf89698f32b0d8e7d3ec2fea922c0
intro2ai <introduction2ai@gmail.com>
2025-06-20 15:46:08 +0530

    for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+// Define these at the top for clarity
+const repo = 'excercise-equilibrium-master'; // YOUR REPOSITORY NAME
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   // Configure Next.js to produce a static site
   output: 'export',
   
+  // Set the basePath for GitHub Pages
+  basePath: isProd ? `/${repo}` : '',
+
+  // Set the assetPrefix for GitHub Pages
+  assetPrefix: isProd ? `/${repo}/` : '',
+  
   // Required for static export with external images.
-  // This disables Next.js's default image optimization.
   images: {
     unoptimized: true,
   },
